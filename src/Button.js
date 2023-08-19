7d5ff0470660db4e5a606844cfd9b3f9520a196a
Michal Izdebski <Theetescoo@gmail.com>
2023-08-19 11:23:53 +0200

    test if branch rule is applied

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+export default function TestButton(){
+    return (
+        <div>
+            <h1>Welcome to my app 2 test</h1>
+            <MyButton />
+        </div>
+    );
+}
