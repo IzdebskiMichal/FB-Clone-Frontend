706383b7a8184c50dc7cb65d175e0e2bd30208a7
Michal Izdebski <Theetescoo@gmail.com>
2023-08-24 21:18:28 +0200

    added a button + sorted out basic behaviour for it.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   HomePage,
   LeftPanel,
@@ -8,16 +8,45 @@ import {
   LeftPanelDiv,
   LeftPanelLink,
   ProfilePic,
+  Button,
 } from "../components/Home/HomeElement";
 import List from "../components/List";
 import profilePic from "../images/profile-picture.png";
 
 function Home(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggle() {
+    setIsOpen((isOpen) => !isOpen);
+    console.log(isOpen);
+  }
+
+  function HiddenElement() {
+    return (
+      <li>
+        <LeftPanelLink to="/friends">
+          <LeftPanelDiv>
+            <ProfilePic src={profilePic} alt="profile picture" />
+            Hidden
+          </LeftPanelDiv>
+        </LeftPanelLink>
+      </li>
+    );
+  }
+
   return (
     <>
       <HomePage>
         <LeftPanel>
           <Ul>
+            <li>
+              <LeftPanelLink to="/profile">
+                <LeftPanelDiv>
+                  <ProfilePic src={profilePic} alt="profile picture" />
+                  Michał Izdebski
+                </LeftPanelDiv>
+              </LeftPanelLink>
+            </li>
             <li>
               <LeftPanelLink to="/friends">
                 <LeftPanelDiv>
@@ -27,8 +56,41 @@ function Home(props) {
               </LeftPanelLink>
             </li>
             <li>
-              <div>Dupa</div>
+              <LeftPanelLink to="/friends">
+                <LeftPanelDiv>
+                  <ProfilePic src={profilePic} alt="profile picture" />
+                  Friends
+                </LeftPanelDiv>
+              </LeftPanelLink>
             </li>
+            <li>
+              <LeftPanelLink to="/friends">
+                <LeftPanelDiv>
+                  <ProfilePic src={profilePic} alt="profile picture" />
+                  Friends
+                </LeftPanelDiv>
+              </LeftPanelLink>
+            </li>
+            <li>
+              <LeftPanelLink to="/friends">
+                <LeftPanelDiv>
+                  <ProfilePic src={profilePic} alt="profile picture" />
+                  Friends
+                </LeftPanelDiv>
+              </LeftPanelLink>
+            </li>
+            {isOpen && <HiddenElement />}
+            <li>
+              <Button onClick={toggle}>
+                <LeftPanelDiv>
+                  <ProfilePic src={profilePic} alt="profile picture" />
+                  Zobacz więcej
+                </LeftPanelDiv>
+              </Button>
+            </li>
+
+            {/* {console.log(isOpen)}
+            {<HiddenElement /> && isOpen} */}
           </Ul>
         </LeftPanel>
         <MiddlePanel>
@@ -39,4 +101,5 @@ function Home(props) {
     </>
   );
 }
+
 export default Home;
