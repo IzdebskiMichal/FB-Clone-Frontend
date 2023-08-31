4fe1fed42c44c41ef87a26e88542b5bdad8dab14
Michal Izdebski <Theetescoo@gmail.com>
2023-09-01 00:01:28 +0200

    starting building tic tac toe

---
+++ src/components/TicTacToe/TicTacToe.js
@@ -0,0 +1,8 @@
+export default function Sqare() {
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
--- src/pages/games.js
+++ src/pages/games.js
@@ -1,7 +1,9 @@
 import React from "react";
+import "../components/TicTacToe/styles.css";
+import TicTacToe from "../components/TicTacToe/TicTacToe";
 
 function Games() {
-  return <div>Games</div>;
+  return <TicTacToe />;
 }
 
 export default Games;
